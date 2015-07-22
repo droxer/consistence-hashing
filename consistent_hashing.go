84887a700e6b052b5eb434072ba3afa2bbe6050d
He Fei <droxer.he@gmail.com>
2015-07-22 22:16:18 +0800

    rename again.

--- consistent_hashing.go
+++ consistent_hashing.go
@@ -10,7 +10,7 @@ type hash func(data []byte) uint32
 type Map struct {
 	hashFunc    hash
 	numReplicas int
-	keys        []int
+	nodes       []int
 	hashMap     map[int]string
 }
 
@@ -22,42 +22,42 @@ func NewMap(numReplicas int, hashFunc hash) *Map {
 	}
 }
 
-func (m *Map) Add(keys ...string) {
-	for _, key := range keys {
+func (m *Map) Add(nodes ...string) {
+	for _, node := range nodes {
 		for i := 0; i < m.numReplicas; i++ {
-			hash := int(m.hashFunc([]byte(strconv.Itoa(i) + key)))
-			m.keys = append(m.keys, hash)
-			m.hashMap[hash] = key
+			hash := int(m.hashFunc([]byte(strconv.Itoa(i) + node)))
+			m.nodes = append(m.nodes, hash)
+			m.hashMap[hash] = node
 		}
 	}
 
-	sort.Ints(m.keys)
+	sort.Ints(m.nodes)
 }
 
-func (m *Map) Remove(keys ...string) {
-	for _, key := range keys {
+func (m *Map) Remove(nodes ...string) {
+	for _, node := range nodes {
 		for i := 0; i < m.numReplicas; i++ {
-			hash := int(m.hashFunc([]byte(strconv.Itoa(i) + key)))
-			idx := sort.Search(len(m.keys), func(i int) bool {
-				return m.keys[i] >= hash
+			hash := int(m.hashFunc([]byte(strconv.Itoa(i) + node)))
+			idx := sort.Search(len(m.nodes), func(i int) bool {
+				return m.nodes[i] >= hash
 			})
-			m.keys = append(m.keys[:idx], m.keys[idx+1:]...)
+			m.nodes = append(m.nodes[:idx], m.nodes[idx+1:]...)
 			delete(m.hashMap, hash)
 		}
 	}
 }
 
 func (m *Map) Get(key string) string {
-	if len(m.keys) == 0 {
+	if len(m.nodes) == 0 {
 		return ""
 	}
 
 	hash := int(m.hashFunc([]byte(key)))
-	idx := sort.Search(len(m.keys), func(i int) bool { return m.keys[i] >= hash })
+	idx := sort.Search(len(m.nodes), func(i int) bool { return m.nodes[i] >= hash })
 
-	if idx == len(m.keys) {
+	if idx == len(m.nodes) {
 		idx = 0
 	}
 
-	return m.hashMap[m.keys[idx]]
+	return m.hashMap[m.nodes[idx]]
 }
